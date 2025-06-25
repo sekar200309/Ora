7e573d3c56bb06d4370f626a2ef4c1d45d4d3c1b
sekar200309 <sekardurai142003@gmail.com>
2025-06-25 22:41:06 +0530

    cors allow for the frontend

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const app = express();
 connectDB();
 
 app.use(cors());
+
+app.use(cors({
+  origin: "https://ora09.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
